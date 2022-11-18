1508f9debc452456440358fea2b2394f906abe03
saadahmad888 <saadbhatti888@gmail.com>
2022-11-19 02:29:09 +0500

    Updated Code Knjr

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -2,9 +2,11 @@
 <html lang="en">
 <head>
     @include('frontend.partials._header')
-    
+
 </head>
 <body>
+    <div id="cursor"></div>
+    <div id="cursor-border"></div>
     <div class="body_wrapper">
         {{-- Website Navbar will be Here --}}
         @include('frontend.partials._navbar')
--- resources/views/frontend/partials/_navbar.blade.php
+++ resources/views/frontend/partials/_navbar.blade.php
@@ -1,6 +1,8 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-transparent">
+<nav class="navbar navbar-expand-lg navbar-light bg-transparent">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#">Tools & Seo</a>
+        <a class="navbar-brand ms-lg-4 ms-md-0 ms-sm-0" href="#">
+            <img width="160px" src="{{asset('frontend/images/logo-2.png')}}" alt="">
+        </a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
             aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
